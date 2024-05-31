15fb86f6f39879e6517170932cc020b2c7bd3b7c
dopamine <coderzyou@gmail.com>
2024-05-31 18:05:01 +0800

    wip

--- docs/.vitepress/utils/highlight.ts
+++ docs/.vitepress/utils/highlight.ts
@@ -3,10 +3,8 @@ import chalk from 'chalk'
 import escapeHtml from 'escape-html'
 import prism from 'prismjs'
 import consola from 'consola'
-
 // prism is listed as actual dep so it's ok to require
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const loadLanguages = require('prismjs/components/index')
+import loadLanguages from 'prismjs/components/index'
 
 // required to make embedded highlighting work...
 loadLanguages(['markup', 'css', 'javascript'])
--- docs/.vitepress/vitepress/utils/index.ts
+++ docs/.vitepress/vitepress/utils/index.ts
@@ -1,25 +1,56 @@
-import {
-  endingSlashRE,
-  isActive,
-  isExternal,
-  normalize,
-} from 'vitepress/dist/client/theme-default/utils'
-
 import type { Route } from 'vitepress'
 
 export * from './colors'
 
-export {
-  isArray,
-  isNullish,
-  isExternal,
-  isActive,
-  normalize,
-  joinUrl,
-  ensureEndingSlash,
-  ensureStartingSlash,
-  removeExtention,
-} from 'vitepress/dist/client/theme-default/utils'
+export const hashRE = /#.*$/
+export const extRE = /(index)?\.(md|html)$/
+export const endingSlashRE = /\/$/
+export const outboundRE = /^[a-z]+:/i
+export function isNullish(value) {
+  return value === null || value === undefined
+}
+export function isArray(value) {
+  return Array.isArray(value)
+}
+export function isExternal(path) {
+  return outboundRE.test(path)
+}
+export function isActive(route, path) {
+  if (path === undefined) {
+    return false
+  }
+  const routePath = normalize(`/${route.data.relativePath}`)
+  const pagePath = normalize(path)
+  return routePath === pagePath
+}
+export function normalize(path) {
+  return decodeURI(path).replace(hashRE, '').replace(extRE, '')
+}
+export function joinUrl(base, path) {
+  const baseEndsWithSlash = base.endsWith('/')
+  const pathStartsWithSlash = path.startsWith('/')
+  if (baseEndsWithSlash && pathStartsWithSlash) {
+    return base.slice(0, -1) + path
+  }
+  if (!baseEndsWithSlash && !pathStartsWithSlash) {
+    return `${base}/${path}`
+  }
+  return base + path
+}
+
+export function ensureStartingSlash(path) {
+  return /^\//.test(path) ? path : `/${path}`
+}
+export function ensureEndingSlash(path) {
+  return /(\.html|\/)$/.test(path) ? path : `${path}/`
+}
+/**
+ * Remove `.md` or `.html` extention from the given path. It also converts
+ * `index` to slush.
+ */
+export function removeExtention(path) {
+  return path.replace(/(index)?(\.(md|html))?$/, '') || '/'
+}
 
 export function utoa(data: string): string {
   return btoa(unescape(encodeURIComponent(data)))
